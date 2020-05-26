eda54357e8ef32f7f428673557f24903512875da
dax <daigo1289@gmail.com>
2020-05-27 00:29:04 +0900

    1-7

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+
 name = 'base'
 
 urlpatterns = [
-    path('', views.top, name='top'),
+    # path('', views.top, name='top'),
+    path('', views.TopView.as_view(), name='top'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.template import loader
 
+'''
 def top(request):
     #template = loader.get_template('base/top.html')
     ctx = {'title': 'Django学習ちゃんねる(仮)'}
     #return HttpResponse(template.render(ctx, request))
     return render(request, 'base/top.html', ctx)
+'''
+from builtins import super
+
+from django.views.generic import TemplateView
+
+class TopView(TemplateView):
+    template_name = 'base/top.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['title'] = 'IT学習ちゃんねる(仮)'
+        return ctx

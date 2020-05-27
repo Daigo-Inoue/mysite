62042e533c1715eccf55287d852e3b6bfc357643
dax <daigo1289@gmail.com>
2020-05-27 16:10:57 +0900

    1-14

--- thread/urls.py
+++ thread/urls.py
@@ -1,9 +1,10 @@
-
 from django.urls import path
 
 from . import views
+
 app_name = 'thread'
 
 urlpatterns = [
     path('<int:pk>/', views.TopicDetailView.as_view(), name='topic'),
+    path('create_topic/', views.topic_create, name='create_topic'),
 ]
